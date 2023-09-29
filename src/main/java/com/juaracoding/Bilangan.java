4d470f647d7752d0b7321ee39fbe542733312f79
Koswara <koswara.c79@gmail.com>
2023-09-29 21:25:11 +0700

    Feature Salary Test

---
+++ src/main/java/com/juaracoding/Bilangan.java
@@ -0,0 +1,13 @@
+package com.juaracoding;
+
+public class Bilangan {
+
+    public String cekGenapGanjil(int angka){
+        if(angka % 2 == 0){
+            return "genap";
+        } else {
+            return "ganjil";
+        }
+    }
+
+}
---
+++ src/test/java/com/juaracoding/SalaryTest.java
@@ -0,0 +1,7 @@
+package com.juaracoding;
+
+public class SalaryTest {
+    // ketika gaji >= 7000
+
+    // ketika gaji < 7000
+}
